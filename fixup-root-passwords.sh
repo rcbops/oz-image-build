52b301d9b3541215d26b8a6767da4817744ddbe6
Joseph W. Breu <breu@breu.org>
2011-12-22 22:04:14 -0600

    .

--- fixup-root-passwords.sh
+++ fixup-root-passwords.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 FNAME=$1
 NEW_ROOT="`pwgen -s 24 -1`"
+COUNT="`grep -c 'ROOT-PW_CHANGE-ME!!!' $FNAME`"
 sed -i 's/ROOT-PW_CHANGE-ME!!!/'$NEW_ROOT'/g' $FNAME
-echo "root password for instance is $NEW_ROOT"
+if [ $COUNT -gt 0 ]; then
+    echo "root password for instance is $NEW_ROOT"
+fi
