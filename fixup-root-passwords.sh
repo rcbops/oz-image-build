fe28c18985a955c2258926d0fd89dc9f2ecab4a7
Joseph W. Breu <breu@breu.org>
2012-01-05 16:47:55 -0600

    iterate

--- fixup-root-passwords.sh
+++ fixup-root-passwords.sh
@@ -5,7 +5,9 @@ NEW_ROOT="`pwgen -s 24 -1`"
 CUR_ROOT=`grep rootpw $FNAME | awk -F'[<|>]' '{print $3}'`
 if [ "$CUR_ROOT" == 'ROOT-PW_CHANGE-ME!!!' ]; then
     sed -i 's/ROOT-PW_CHANGE-ME!!!/'$NEW_ROOT'/g' $FNAME
-    echo "$NEW_ROOT"
-else
-    echo "$CUR_ROOT"
 fi
+
+#    echo "$NEW_ROOT"
+#else
+#    echo "$CUR_ROOT"
+#fi
